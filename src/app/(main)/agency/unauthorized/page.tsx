2ee2ff176ff9949089349a063bb5cee495a5b799
Sandile-Repos <sandile.developer@gmail.com>
2024-03-07 14:56:12 +0200

    add sidebar

---
+++ src/app/(main)/agency/unauthorized/page.tsx
@@ -0,0 +1,9 @@
+import Unauthorized from "@/components/unauthorized";
+import React from "react";
+
+type Props = {};
+const page = (props: Props) => {
+  return <Unauthorized />;
+};
+
+export default page;
