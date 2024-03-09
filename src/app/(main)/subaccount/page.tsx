769db3f78a70d7f07f21a1ee2739e5f00b183a7f
Sandile-Repos <sandile.developer@gmail.com>
2024-03-09 23:53:36 +0200

    Add the Sub Account Section

--- src/app/(main)/subaccount/page.tsx
+++ src/app/(main)/subaccount/page.tsx
@@ -1,7 +1,44 @@
+import Unauthorized from "@/components/unauthorized";
+import { getAuthUserDetails, verifyAndAcceptInvitation } from "@/lib/queries";
+import { redirect } from "next/navigation";
 import React from "react";
 
-const Page = () => {
-  return <div>Sub Account</div>;
+type Props = {
+  //this will come from stripe after user have connected to their stripe account
+  searchParams: { state: string; code: string };
 };
 
-export default Page;
+const SubAccountMainPage = async ({ searchParams }: Props) => {
+  const agencyId = await verifyAndAcceptInvitation();
+
+  if (!agencyId) {
+    return <Unauthorized />;
+  }
+
+  const user = await getAuthUserDetails();
+  if (!user) return;
+
+  const getFirstSubaccountWithAccess = user.Permissions.find(
+    (permission) => permission.access === true
+  );
+  console.log("?????", searchParams);
+  console.log(".....", getFirstSubaccountWithAccess);
+
+  if (searchParams.state) {
+    //note three underscores(___) not two
+    const statePath = searchParams.state.split("___")[0];
+    const stateSubaccountId = searchParams.state.split("___")[1];
+    if (!stateSubaccountId) return <Unauthorized />;
+    return redirect(
+      `/subaccount/${stateSubaccountId}/${statePath}?code=${searchParams.code}`
+    );
+  }
+
+  if (getFirstSubaccountWithAccess) {
+    return redirect(`/subaccount/${getFirstSubaccountWithAccess.subAccountId}`);
+  }
+
+  return <Unauthorized />;
+};
+
+export default SubAccountMainPage;
